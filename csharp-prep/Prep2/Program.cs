099dd8c22cb1ef674e97c49af48e6f0e07b96c96
example@email.com <agustin.morinico@gmail.com>
2023-05-18 22:27:38 -0300

    Prep 2 Recovery

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your porcentage grade? ");
+        string userValue = Console.ReadLine();
+        int userValueNum = int.Parse(userValue);
+        string letterGrade = "";
+        string pass = "";
+
+        if (userValueNum >= 90)
+        {
+            letterGrade = "A";
+            pass = "passed";
+        }
+
+        else if (userValueNum >= 80)
+        {
+            letterGrade = "B";
+            pass = "passed";
+        }
+
+        else if (userValueNum >= 7)
+        {
+            letterGrade = "C";
+            pass = "passed";
+        }
+
+        else if (userValueNum >= 60)
+        {
+            letterGrade = "D";
+            pass = "not passed";
+        }
+
+        else
+        {
+            letterGrade = "F";
+            pass = "not passed";
+        }
+
+        Console.WriteLine($"You have scored a {letterGrade}.");
+        Console.WriteLine($"You have {pass}.");
+        
     }
 }
