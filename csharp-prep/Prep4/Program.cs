308f1413d756dad0016ab00c268c44f3060f241f
example@email.com <agustin.morinico@gmail.com>
2023-05-23 09:24:42 -0300

    prep 4 complete with stretch

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int userNum;
+        int numberSum = 0;
+        float numberAvg;
+        int maxNumber;
+        List<int> numbers = new List<int>();
+        
+
+        do
+        {
+            Console.Write("Enter number: ");
+            string userInput = Console.ReadLine();
+            userNum = int.Parse(userInput);
+            if (userNum != 0)
+            {
+                numbers.Add(userNum);
+            }
+
+        } while (userNum != 0);
+
+        foreach (int num in numbers)
+        {
+            numberSum = numberSum + num;
+        }
+
+        Console.WriteLine($"The sum is: {numberSum}");
+
+        numberAvg = ((float)numberSum) / numbers.Count;
+
+        Console.WriteLine($"The average is: {numberAvg}");
+
+        maxNumber = numbers.Max();
+
+        Console.WriteLine($"The largest number is: {maxNumber}");
+
     }
 }
