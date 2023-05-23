219aff81ee46cf777d9bc5624a58de58204f01fb
example@email.com <agustin.morinico@gmail.com>
2023-05-23 08:38:06 -0300

    prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Guess the magic number!");
+        string playAgain = "y";
+
+        while (playAgain == "y")
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+            
+            int guessNum = 0;
+            int guessCounter = 0;
+
+            while (guessNum != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                string userGuess = Console.ReadLine();
+                guessNum = int.Parse(userGuess);
+                guessCounter = guessCounter + 1;
+
+                if (guessNum < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+
+                }
+                else if (guessNum > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+            }
+
+            Console.WriteLine($"You guessed it in {guessCounter} times!");
+            Console.Write("Would you like to play again?(y/n) ");
+            playAgain = Console.ReadLine();
+        }
     }
 }
