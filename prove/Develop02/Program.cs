373bcb4644a2664aa0a92ce84e77ee78a8d98e6f
example@email.com <agustin.morinico@gmail.com>
2023-05-25 10:34:46 -0300

    Classes ready

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -6,4 +6,34 @@ class Program
     {
         Console.WriteLine("Hello Develop02 World!");
     }
+
+    static void Menu()
+    {
+
+    }
+}
+
+public class Journal
+{
+    public List<Entry> _entries = new List<Entry>();
+
+    public void DisplayEntries()
+    {
+        foreach (Entry entry in _entries)
+        {
+            entry.Display();
+        }
+    }
+}
+
+public class Entry
+{
+    public string _date;
+    public string _prompt;
+    public string _entry;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_date} {_prompt} {_entry}");
+    }
 }
